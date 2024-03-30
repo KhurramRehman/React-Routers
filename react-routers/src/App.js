379d240a4bc2edebe6238597e718015b55cc3b28
Khurram Ur Rehman <khurram.rehman@hotmail.com>
2024-03-30 17:50:16 +0000

    upto lesson 5 completed

--- react-routers/src/App.js
+++ react-routers/src/App.js
@@ -1,13 +1,16 @@
 import {
   createBrowserRouter,
   Route,
-  NavLink,
   createRoutesFromElements,
   RouterProvider
 } from 'react-router-dom'
 import Home from './pages/Home';
 import About from './pages/About';
 import RootLayout from './Layouts/RootLayout';
+import HelpLayout from './Layouts/HelpLayout';
+import Faq from './pages/help/Faq';
+import Contact from './pages/help/Contact';
+import NotFound from './pages/NotFound';
 
 
 const router = createBrowserRouter(
@@ -15,6 +18,12 @@ const router = createBrowserRouter(
     <Route path='/' element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path='about' element={<About />} />
+      <Route path='help' element={<HelpLayout />}>
+        <Route path='faq' element={<Faq />} />
+        <Route path='contact' element={<Contact />} />
+      </Route>
+
+      <Route path='*' element={<NotFound />} />
     </Route>
   ))
 
