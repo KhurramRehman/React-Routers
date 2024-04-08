a6f27246a1c0428c04dbcf6d46d796c5dddcab50
KhurramRehman <khurram.rehman@hotmail.com>
2024-04-08 14:50:35 +0100

    lesson 9 completed making breadcrumbs

--- react-routers/src/pages/careers/Careers.js
+++ react-routers/src/pages/careers/Careers.js
@@ -20,7 +20,7 @@ export default Careers;
 //loader function
 
 export const careersLoader =  async () => {
-    const res = await fetch('http://localhost:4000/careerss');
+    const res = await fetch('http://localhost:4000/careers');
 
     if(!res.ok) {
         throw Error('Could not fetch the careers');
